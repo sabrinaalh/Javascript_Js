ecaa9fe63e49e7081805708510996c1b945c58f2
Sabrina Almeida <109253438+sabrinaalh@users.noreply.github.com>
2023-02-10 12:00:02 -0300

    Upp

--- TheCompleteJavascriptCourse/challenges/challenge06.js
+++ TheCompleteJavascriptCourse/challenges/challenge06.js
@@ -1,12 +1,32 @@
 const calcTips = dollar =>{
-    if(dollar >= 50 && dollar < 300){
-        const tip = dollar/100*15;
+    let tip = 0;
+    if(dollar >= 50 && dollar <= 300){
+        tip = dollar*0.15;
     }else{
-        const tip = dollar/100*20;
+         tip = dollar*0.20;
     }
     return tip;
 }
 
-console.log(calcTips(bill[0]));
-
 const bill = [125, 555, 44];
+const tips = [calcTips(bill[0]),calcTips(bill[1]),calcTips(bill[2])];
+console.log(tips);
+
+const total = [bill[0] + calcTips(bill[0]), bill[1]+ calcTips(bill[1]), bill[2] + calcTips(bill[2])];
+
+
+
+for(let i = 0; i < 3; i++){
+    switch(i){
+        case 0:
+            console.log("Primeira conta: ");
+            break;
+        case 1:
+            console.log("Segunda conta: ");
+            break;
+        case 2:
+            console.log("Terceira conta: ");
+            break;
+    }
+    console.log(`Valor da compra: R$${bill[i]}  + Valor da gorjeta: R$${calcTips(bill[i])}. TOTAL: R$${total[i]}`);
+}
